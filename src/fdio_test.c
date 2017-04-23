0999ea90aacefce57373c2f21a7b116ad197c90f
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-04-23 11:14:14 +0200

    fdio: Don't define _XOPEN_SOURCE in source file

--- src/fdio_test.c
+++ src/fdio_test.c
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define _XOPEN_SOURCE 500 /* Needed for pwrite */
-
 #include "fdio_test.h"
 #include <errno.h>
 #include <fcntl.h>
