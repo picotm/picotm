649feecde69fad9806e0eed4d309e929bad061ac
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-04-24 19:34:44 +0200

    Clean up implementation of |struct log|

--- lib/src/log.h
+++ lib/src/log.h
@@ -2,53 +2,57 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef LOG_H
-#define LOG_H
+#pragma once
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+struct event;
 struct module;
 
 /**
- * \brief The log is the core part of the framework. It holds the events of a
- *        transaction.
+ * \brief The log holds the events of a transaction.
  */
 struct log {
-    struct event *eventtab; /** \brief Table of transction's events */
-    size_t        eventtablen; /** \brief Number of events */
-    size_t        eventtabsiz; /** \brief Maximum number of events */
+
+    /** Table of transaction's events. */
+    struct event* eventtab;
+
+    /** Number of events in the log. */
+    size_t eventtablen;
+
+    /** Maximum number of events. */
+    size_t eventtabsiz;
 };
 
 /**
- * \brief Init log
+ * Init log.
  */
 int
-log_init(struct log *log);
+log_init(struct log* self);
 
 /**
- * \brief Uninit log
+ * Uninit log.
  */
-int
-log_uninit(struct log *log);
+void
+log_uninit(struct log* self);
 
 /**
- * \brief Inject an event into log
+ * Inject an event into log.
  */
 int
-log_inject_event(struct log *log, unsigned long module, unsigned long call,
+log_inject_event(struct log* self, unsigned long module, unsigned long call,
                  uintptr_t cookie);
 
 /**
- * \brief Apply events in the log
+ * Apply events in the log.
  */
 int
-log_apply_events(struct log *log, struct module* module, int noundo);
+log_apply_events(struct log* self, const struct module* module, bool noundo);
 
 /**
- * \brief Undo events in the log
+ * Undo events in the log.
  */
 int
-log_undo_events(struct log *log, struct module* module, int noundo);
-
-#endif
+log_undo_events(struct log* self, const struct module* module, bool noundo);
