1c0da34ff36e5229bff6986495b4b9b620cebae1
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-04-26 13:05:16 +0200

    libc: Move public error-handling functions to libc.c
    With this patch, errno.c only contains errno-specific code. All other
    code related to error handling is located in libc.c or in the directory
    error/.

--- lib/modules/libc/src/libc.c
+++ lib/modules/libc/src/libc.c
@@ -3,6 +3,36 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "picotm/picotm-libc.h"
+#include "error/module.h"
+
+/*
+ * Error handling
+ */
+
+PICOTM_EXPORT
+void
+picotm_libc_save_errno()
+{
+    error_module_save_errno();
+}
+
+PICOTM_EXPORT
+void
+picotm_libc_set_error_recovery(enum picotm_libc_error_recovery recovery)
+{
+    return error_module_set_error_recovery(recovery);
+}
+
+PICOTM_EXPORT
+enum picotm_libc_error_recovery
+picotm_libc_get_error_recovery()
+{
+    return error_module_get_error_recovery();
+}
+
+/*
+ * File I/O
+ */
 
 static enum picotm_libc_cc_mode g_file_type_cc_mode[] = {
     PICOTM_LIBC_CC_MODE_NOUNDO,
