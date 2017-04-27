014612ab5617b2edf4bb338c225dc540cbf4478c
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-04-27 14:17:19 +0200

    libc: Clean up include statements in fs/

--- lib/modules/libc/src/fs/fchdirop.c
+++ lib/modules/libc/src/fs/fchdirop.c
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "fchdirop.h"
 #include <assert.h>
 #include <stdio.h>
-#include "fchdirop.h"
 
 int
 fchdirop_init(struct fchdirop *fchdirop, int oldcwd, int newcwd)
