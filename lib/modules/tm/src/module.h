9f097b8807b907b034fd83c208b1735caf48e357
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-05-10 10:33:05 +0200

    tm: Move module interfaces to module.{c,h}
    This patch separates the public interface of the TM module from its
    implementation. The latter is now located in module.{c,h}.

---
+++ lib/modules/tm/src/module.h
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+void
+tm_module_load(uintptr_t addr, void* buf, size_t siz);
+
+void
+tm_module_store(uintptr_t addr, const void* buf, size_t siz);
+
+void
+tm_module_loadstore(uintptr_t laddr, uintptr_t saddr, size_t siz);
+
+void
+tm_module_privatize(uintptr_t addr, size_t siz, unsigned long flags);
+
+void
+tm_module_privatize_c(uintptr_t addr, int c, unsigned long flags);
