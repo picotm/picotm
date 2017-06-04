44705c20b34c30382f057848ace6e5cfadaa4b70
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-06-04 11:03:46 +0200

    libc: Replace GCC atomic builtins with C11 atomic operations

--- lib/modules/libc/src/libc.c
+++ lib/modules/libc/src/libc.c
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "picotm/picotm-libc.h"
+#include <stdatomic.h>
 #include "error/module.h"
 
 /*
@@ -34,38 +35,40 @@ picotm_libc_get_error_recovery()
  * File I/O
  */
 
-static enum picotm_libc_cc_mode g_file_type_cc_mode[] = {
+static _Atomic enum picotm_libc_cc_mode g_file_type_cc_mode[] = {
     PICOTM_LIBC_CC_MODE_NOUNDO,
     PICOTM_LIBC_CC_MODE_2PL
 };
 
-static enum picotm_libc_validation_mode g_validation_mode;
+static _Atomic enum picotm_libc_validation_mode g_validation_mode;
 
 PICOTM_EXPORT
 void
 picotm_libc_set_file_type_cc_mode(enum picotm_libc_file_type file_type,
                                   enum picotm_libc_cc_mode cc_mode)
 {
-    return __atomic_store_n(g_file_type_cc_mode + file_type, cc_mode, __ATOMIC_RELEASE);
+    atomic_store_explicit(g_file_type_cc_mode + file_type, cc_mode,
+                          memory_order_release);
 }
 
 PICOTM_EXPORT
 enum picotm_libc_cc_mode
 picotm_libc_get_file_type_cc_mode(enum picotm_libc_file_type file_type)
 {
-    return __atomic_load_n(g_file_type_cc_mode + file_type, __ATOMIC_ACQUIRE);
+    return atomic_load_explicit(g_file_type_cc_mode + file_type,
+                                memory_order_acquire);
 }
 
 PICOTM_EXPORT
 void
 picotm_libc_set_validation_mode(enum picotm_libc_validation_mode val_mode)
 {
-    __atomic_store_n(&g_validation_mode, val_mode, __ATOMIC_RELEASE);
+    atomic_store_explicit(&g_validation_mode, val_mode, memory_order_release);
 }
 
 PICOTM_EXPORT
 enum picotm_libc_validation_mode
 picotm_libc_get_validation_mode()
 {
-    return __atomic_load_n(&g_validation_mode, __ATOMIC_ACQUIRE);
+    return atomic_load_explicit(&g_validation_mode, memory_order_acquire);
 }
