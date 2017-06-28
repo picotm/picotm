a53025935499c7a59fd7c47d55eb1195f8a37ae0
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-06-28 14:07:49 +0200

    tm: Implement |struct tm_frame_map| with |struct picotm_shared_treemap|

--- lib/modules/tm/src/framemap.c
+++ lib/modules/tm/src/framemap.c
@@ -18,16 +18,25 @@
  */
 
 #include "framemap.h"
+#include <assert.h>
+#include <limits.h>
 #include <picotm/picotm-error.h>
-#include <stdatomic.h>
-#include <stdbool.h>
+#include <picotm/picotm-lib-array.h>
 #include <stdlib.h>
-#include <string.h>
-#include "picotm/picotm-lib-array.h"
+#include "block.h"
+#include "frame.h"
 
 /*
- * Frame table
+ * frame table
  */
+
+#define TM_FRAME_TBL_SIZE_BITS  (10)
+#define TM_FRAME_TBL_SIZE       (1ul << TM_FRAME_TBL_SIZE_BITS)
+#define TM_FRAME_TBL_SIZE_MASK  (TM_FRAME_TBL_SIZE - 1)
+
+struct tm_frame_tbl {
+    struct tm_frame frame[TM_FRAME_TBL_SIZE];
+};
 
 static void
 tm_frame_tbl_init(struct tm_frame_tbl* self, size_t first_block_index)
@@ -40,232 +49,85 @@ tm_frame_tbl_init(struct tm_frame_tbl* self, size_t first_block_index)
     }
 }
 
-/*
- * Frame directory
- */
+static void
+tm_frame_tbl_uninit(struct tm_frame_tbl* self)
+{ }
 
-void
-tm_frame_dir_init(struct tm_frame_dir* self)
+static uintptr_t
+tm_frame_tbl_create(unsigned long long key,
+                 struct picotm_shared_treemap* treemap,
+                 struct picotm_error* error)
 {
-    memset(self->entry, 0, sizeof(self->entry));
+    struct tm_frame_tbl* tbl = malloc(sizeof(*tbl));
+    if (!tbl) {
+        picotm_error_set_error_code(error, PICOTM_OUT_OF_MEMORY);
+        return 0;
+    }
+
+    tm_frame_tbl_init(tbl, key << TM_FRAME_TBL_SIZE_BITS);
+
+    return (uintptr_t)tbl;
+}
+
+static void
+tm_frame_tbl_destroy(uintptr_t value, struct picotm_shared_treemap* treemap)
+{
+    struct tm_frame_tbl* tbl = (struct tm_frame_tbl*)value;
+
+    tm_frame_tbl_uninit(tbl);
+    free(tbl);
 }
 
 /*
- * Frame top-level directory
- */
-
-void
-tm_frame_tld_init(struct tm_frame_tld* self)
-{
-    memset(self->entry, 0, sizeof(self->entry));
-}
-
-/*
- * Frame map
+ * frame map
  */
 
 void
 tm_frame_map_init(struct tm_frame_map* self)
 {
-    tm_frame_tld_init(&self->tld);
-}
+    /* The number of bits in an address that are handled by the
+     * treemap. The lowest bits in each address index into the block
+     * and frame. The higher bits index into the treemap directories.
+     */
+    unsigned long key_nbits =
+        (sizeof(void*) * CHAR_BIT) -
+            (TM_FRAME_TBL_SIZE_BITS + TM_BLOCK_SIZE_BITS);
 
-static bool
-reached_tbl(size_t shiftbits)
-{
-    return shiftbits <= (TM_FRAME_TBL_SIZE_BITS + TM_BLOCK_SIZE_BITS);
-}
-
-static void
-cleanup_tbl(struct tm_frame_tbl* tbl)
-{
-    free(tbl);
-}
-
-static void
-cleanup_dir(struct tm_frame_dir* dir, size_t shiftbits)
-{
-    shiftbits -= TM_FRAME_DIR_SIZE_BITS;
-
-    atomic_uintptr_t* beg = picotm_arraybeg(dir->entry);
-    const atomic_uintptr_t* end = picotm_arrayend(dir->entry);
-
-    if (reached_tbl(shiftbits)) {
-        while (beg < end) {
-            if (*beg) {
-                cleanup_tbl((struct tm_frame_tbl*)(*beg));
-            }
-            ++beg;
-        }
-    } else {
-        while (beg < end) {
-            if (*beg) {
-                cleanup_dir((struct tm_frame_dir*)(*beg), shiftbits);
-            }
-            ++beg;
-        }
-    }
-    free(dir);
-}
-
-static void
-cleanup_tld(struct tm_frame_tld* tld, size_t shiftbits)
-{
-    shiftbits -= TM_FRAME_TLD_SIZE_BITS;
-
-    atomic_uintptr_t* beg = picotm_arraybeg(tld->entry);
-    const atomic_uintptr_t* end = picotm_arrayend(tld->entry);
-
-    if (reached_tbl(shiftbits)) {
-        while (beg < end) {
-            if (*beg) {
-                cleanup_tbl((struct tm_frame_tbl*)(*beg));
-            }
-            ++beg;
-        }
-    } else {
-        while (beg < end) {
-            if (*beg) {
-                cleanup_dir((struct tm_frame_dir*)(*beg), shiftbits);
-            }
-            ++beg;
-        }
-    }
-}
-
-static size_t
-initial_shiftbits(void)
-{
-    return (CHAR_BIT * sizeof(void*));
+    picotm_shared_treemap_init(&self->map, key_nbits, 10);
 }
 
 void
 tm_frame_map_uninit(struct tm_frame_map* self)
 {
-    cleanup_tld(&self->tld, initial_shiftbits());
+    picotm_shared_treemap_uninit(&self->map, tm_frame_tbl_destroy);
+}
+
+static unsigned long long
+frame_tbl_offset(uintptr_t addr)
+{
+    return addr >> (TM_FRAME_TBL_SIZE_BITS + TM_BLOCK_SIZE_BITS);
 }
 
 static size_t
-first_block_index(uintptr_t addr)
+frame_tbl_index(uintptr_t addr)
 {
-    return (addr >> (TM_FRAME_TBL_SIZE_BITS + TM_BLOCK_SIZE_BITS)) << TM_FRAME_TBL_SIZE_BITS;
-}
-
-static size_t
-tbl_index(uintptr_t addr, size_t shiftbits)
-{
-    shiftbits -= TM_FRAME_TBL_SIZE_BITS;
-    return (addr >> shiftbits) & TM_FRAME_TBL_SIZE_MASK;
-}
-
-static size_t
-next_dir_index(uintptr_t addr, size_t* shiftbits)
-{
-    *shiftbits -= TM_FRAME_DIR_SIZE_BITS;
-    return (addr >> (*shiftbits)) & TM_FRAME_DIR_SIZE_MASK;
-}
-
-static size_t
-next_tld_index(uintptr_t addr, size_t* shiftbits)
-{
-    *shiftbits -= TM_FRAME_TLD_SIZE_BITS;
-    return (addr >> (*shiftbits)) & TM_FRAME_TLD_SIZE_MASK;
-}
-
-static struct tm_frame_dir*
-load_dir(atomic_uintptr_t* entry, struct picotm_error* error)
-{
-    struct tm_frame_dir* dir =
-        (struct tm_frame_dir*)atomic_load_explicit(entry, memory_order_acquire);
-    if (dir) {
-        return dir;
-    }
-
-    dir = malloc(sizeof(*dir));
-    if (!dir) {
-        picotm_error_set_error_code(error, PICOTM_OUT_OF_MEMORY);
-        return NULL;
-    }
-    tm_frame_dir_init(dir);
-
-    uintptr_t existing = 0;
-    bool success =
-        atomic_compare_exchange_strong_explicit(entry,
-                                                &existing,
-                                                (uintptr_t)dir,
-                                                memory_order_acq_rel,
-                                                memory_order_acquire);
-    if (!success) {
-        /* A concurrent transaction already installed the
-         * directory. We free our instance and use the existing
-         * one. */;
-        free(dir);
-        dir = (struct tm_frame_dir*)existing;
-    }
-
-    return dir;
-}
-
-static struct tm_frame_tbl*
-load_tbl(atomic_uintptr_t* entry, uintptr_t addr, struct picotm_error* error)
-{
-    struct tm_frame_tbl* tbl =
-        (struct tm_frame_tbl*)atomic_load_explicit(entry,
-                                                   memory_order_acquire);
-    if (tbl) {
-        return tbl;
-    }
-
-    tbl = malloc(sizeof(*tbl));
-    if (!tbl) {
-        picotm_error_set_error_code(error, PICOTM_OUT_OF_MEMORY);
-        return NULL;
-    }
-    tm_frame_tbl_init(tbl, first_block_index(addr));
-
-    uintptr_t existing = 0;
-    bool success =
-        atomic_compare_exchange_strong_explicit(entry,
-                                                &existing,
-                                                (uintptr_t)tbl,
-                                                memory_order_acq_rel,
-                                                memory_order_acquire);
-    if (!success) {
-        /* A concurrent transaction already installed the
-         * table. We free our instance and use the existing
-         * one. */;
-        free(tbl);
-        tbl = (struct tm_frame_tbl*)existing;
-    }
-
-    return tbl;
+    return (addr >> TM_BLOCK_SIZE_BITS) & TM_FRAME_TBL_SIZE_MASK;
 }
 
 struct tm_frame*
 tm_frame_map_lookup(struct tm_frame_map* self, uintptr_t addr,
                     struct picotm_error* error)
 {
-    size_t shiftbits = initial_shiftbits();
+    assert(self);
 
-    /* Look up frame directory from top-level directory */
-
-    atomic_uintptr_t* entry = self->tld.entry + next_tld_index(addr, &shiftbits);
-
-    /* Look up frame table from directory hierarchy */
-
-    while (!reached_tbl(shiftbits)) {
-        struct tm_frame_dir* dir = load_dir(entry, error);
-        if (picotm_error_is_set(error)) {
-            return NULL;
-        }
-        entry = dir->entry + next_dir_index(addr, &shiftbits);
-    }
-
-    /* Look up frame from frame table */
-
-    struct tm_frame_tbl* tbl = load_tbl(entry, addr, error);
+    uintptr_t value = picotm_shared_treemap_find_value(
+        &self->map, frame_tbl_offset(addr),
+        tm_frame_tbl_create, tm_frame_tbl_destroy,
+        error);
     if (picotm_error_is_set(error)) {
         return NULL;
     }
-    return tbl->frame + tbl_index(addr, shiftbits);
+    struct tm_frame_tbl* tbl = (struct tm_frame_tbl*)value;
+
+    return tbl->frame + frame_tbl_index(addr);
 }
