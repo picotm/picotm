8cb275e8dc8aa81bdc561adcb9129a6fccba6c43
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-06-28 14:07:49 +0200

    tm: Implement |struct tm_frame_map| with |struct picotm_shared_treemap|

--- lib/modules/tm/src/framemap.h
+++ lib/modules/tm/src/framemap.h
@@ -19,11 +19,7 @@
 
 #pragma once
 
-#include <stdatomic.h>
-#include <limits.h>
-#include <stdint.h>
-#include "block.h"
-#include "frame.h"
+#include <picotm/picotm-lib-shared-treemap.h>
 
 /**
  * \cond impl || tm_impl
@@ -33,50 +29,10 @@
  */
 
 struct picotm_error;
-
-/*
- * Frame table
- */
-
-#define TM_FRAME_TBL_SIZE_BITS  (10)
-#define TM_FRAME_TBL_SIZE       (1ul << TM_FRAME_TBL_SIZE_BITS)
-#define TM_FRAME_TBL_SIZE_MASK  (TM_FRAME_TBL_SIZE - 1)
-
-struct tm_frame_tbl {
-    struct tm_frame frame[TM_FRAME_TBL_SIZE];
-};
-
-/*
- * Frame directory
- */
-
-#define TM_FRAME_DIR_SIZE_BITS  (10)
-#define TM_FRAME_DIR_SIZE       (1ul << TM_FRAME_DIR_SIZE_BITS)
-#define TM_FRAME_DIR_SIZE_MASK  (TM_FRAME_DIR_SIZE - 1)
-
-struct tm_frame_dir {
-    atomic_uintptr_t entry[TM_FRAME_DIR_SIZE];
-};
-
-/*
- * Frame top-level directory
- */
-
-#define TM_FRAME_TLD_SIZE_BITS \
-    (((CHAR_BIT * sizeof(void*)) - TM_BLOCK_SIZE_BITS - TM_FRAME_TBL_SIZE_BITS) % TM_FRAME_DIR_SIZE_BITS)
-#define TM_FRAME_TLD_SIZE       (1ul << TM_FRAME_TLD_SIZE_BITS)
-#define TM_FRAME_TLD_SIZE_MASK  (TM_FRAME_TLD_SIZE - 1)
-
-struct tm_frame_tld {
-    atomic_uintptr_t entry[TM_FRAME_TLD_SIZE];
-};
-
-/*
- * Frame map
- */
+struct tm_frame;
 
 struct tm_frame_map {
-    struct tm_frame_tld tld;
+    struct picotm_shared_treemap map;
 };
 
 void
