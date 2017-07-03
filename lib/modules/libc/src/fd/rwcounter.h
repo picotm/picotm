44781f76e36b915ad5dac44eeeed1531331c7f90
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-07-03 09:54:04 +0200

    libc: Add |struct rwcounter|

---
+++ lib/modules/libc/src/fd/rwcounter.h
@@ -0,0 +1,49 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+/**
+ * \cond impl || libc_impl || libc_impl_fd
+ * \ingroup libc_impl
+ * \ingroup libc_impl_fd
+ * \file
+ * \endcond
+ */
+
+struct picotm_error;
+struct picotm_rwlock;
+
+struct rwcounter {
+    unsigned long state;
+};
+
+void
+rwcounter_init(struct rwcounter* self);
+
+void
+rwcounter_rdlock(struct rwcounter* self, struct picotm_rwlock* rwlock,
+                 struct picotm_error* error);
+
+void
+rwcounter_wrlock(struct rwcounter* self, struct picotm_rwlock* rwlock,
+                 struct picotm_error* error);
+
+void
+rwcounter_unlock(struct rwcounter* self, struct picotm_rwlock* rwlock);
