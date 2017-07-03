d3d7f62079bd2f3d8fc67928ddb0b74a5f945683
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-07-03 14:05:52 +0200

    libc: Maintain transactional changes to f/d table in |struct fdtab_tx|

---
+++ lib/modules/libc/src/fd/fdtab_tx.h
@@ -0,0 +1,46 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <picotm/picotm-lib-rwstate.h>
+#include <stdbool.h>
+
+struct fdtab_tx {
+};
+
+void
+fdtab_tx_init(struct fdtab_tx* self);
+
+void
+fdtab_tx_uninit(struct fdtab_tx* self);
+
+struct fd*
+fdtab_tx_ref_fildes(struct fdtab_tx* self, int fildes,
+                    struct picotm_error* error);
+
+/*
+ * Module Interface
+ */
+
+void
+fdtab_tx_update_cc(struct fdtab_tx* self, struct picotm_error* error);
+
+void
+fdtab_tx_clear_cc(struct fdtab_tx* self, struct picotm_error* error);
