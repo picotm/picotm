20e34ccd810bfbc7ed9cdeee909316c1d3bae1c8
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-07-03 14:58:25 +0200

    lib: Add |struct picotm_rwstate| to module-helper library

---
+++ lib/include/picotm/picotm-lib-rwstate.h
@@ -0,0 +1,237 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "compiler.h"
+
+/**
+ * \ingroup group_modules
+ * \file
+ *
+ * \brief Contains `struct picotm_rwstate` and helpers.
+ *
+ * The data structure `struct picotm_rwstate` represents a transaction's
+ * status wrt. shared state.
+ *
+ * For example, shared state might be an integer with an associated
+ * reader/writer lock.
+ *
+ * ~~~ c
+ *      int i;
+ *      struct picotm_rwlock i_lock;
+ *
+ *      i = 0;
+ *      picotm_rwlock_init(&i_lock);
+ * ~~~
+ *
+ * Each transaction accessing the integer `i` contains a reader/writer
+ * state value of type `struct picotm_rwstate`. The transaction has to
+ * initialize the state value with a call to `picotm_rwstate_init()`.
+ *
+ * ~~~ c
+ *      struct picotm_rwstate i_state;
+ *
+ *      picotm_rwstate_init(&i_state);
+ * ~~~
+ *
+ * To safely read the value of resource `i`, a transaction has to acquire
+ * the reader lock `i_lock`.
+ *
+ * ~~~ c
+ *      struct picotm_error error = PICOTM_ERROR_INITIALIZER;
+ *
+ *      picotm_rwstate_try_rdlock(&i_state, &i_lock, &error);
+ *      if (picotm_error_is_set(&error)) {
+ *          // perform recovery
+ *      }
+ *
+ *      // read i
+ * ~~~
+ *
+ * A call to `picotm_rwstate_try_rdlock()` acquires a reader lock and sets
+ * the transaction's reader/writer state accordingly. If the reader lock
+ * could not be acquired, an error is returned. In this case, the transaction
+ * should initiate a recovery.
+ *
+ * To safely write, a transaction has to acquire the writer lock `i_lock`.
+ *
+ * ~~~ c
+ *      picotm_rwstate_try_wrlock(&i_state, &i_lock, &error);
+ *      if (picotm_error_is_set(&error)) {
+ *          // perform recovery
+ *      }
+ *
+ *      // write i
+ * ~~~
+ *
+ * Depending on the transaction's current reader/writer state, the function
+ * `picotm_rwstate_try_wrlock()` automatically updates a reader lock to a
+ * writer lock.
+ *
+ * Reader/writer locks can only be acquired once by each transaction, and
+ * possibly upgraded once by each transaction. The reader/writer state
+ * variable keeps track of the transaction's lock state, so locks can be
+ * acquired and upgraded multiple times.
+ *
+ * ~~~ c
+ *      picotm_rwstate_try_wrlock(&i_state, &i_lock, &error);
+ *      if (picotm_error_is_set(&error)) {
+ *          // perform recovery
+ *      }
+ *      picotm_rwstate_try_rdlock(&i_state, &i_lock, &error);
+ *      if (picotm_error_is_set(&error)) {
+ *          // perform recovery
+ *      }
+ *      picotm_rwstate_try_wrlock(&i_state, &i_lock, &error);
+ *      if (picotm_error_is_set(&error)) {
+ *          // perform recovery
+ *      }
+ * ~~~
+ *
+ * In any case, a reader-writer lock can only be unlocked *once,* using
+ * `picotm_rwstate_unlock()`.
+ * ~~~ c
+ *      picotm_rwstate_unlock(&i_state, &i_lock);
+ * ~~~
+ *
+ * The current reader/writer state can be queried using
+ * `picotm_rwstate_get_status()`, which returns one of
+ * `enum picotm_rwstate_status`. Using `picotm_rwstate_set_status()`, the
+ * status can also be set explicitly. This might be useful when adopting
+ * pre-acquired reader/writer locks, but it's generally preferable to use
+ * reader/writer state interfaces for locking and unlocking.
+ *
+ * Finally, to uninitialize a reader/writer state the transaction has to
+ * call `picotm_rwstate_uninit()`.
+ *
+ * ~~~ c
+ *      picotm_rwstate_uninit(&i_state);
+ * ~~~
+ */
+
+PICOTM_BEGIN_DECLS
+
+struct picotm_error;
+struct picotm_rwlock;
+
+/**
+ * Signals a shared state's status wrt. a transaction
+ */
+enum picotm_rwstate_status {
+    /** The transaction has not acquired the lock. */
+    PICOTM_RWSTATE_UNLOCKED = 0,
+    /** The transaction has acquired a reader lock. */
+    PICOTM_RWSTATE_RDLOCKED,
+    /** The transaction has acquired a writer lock. */
+    PICOTM_RWSTATE_WRLOCKED
+};
+
+/**
+ * Represents shared state that can be read or written by a transaction.
+ */
+struct picotm_rwstate {
+    /**
+     * The shared state's status wrt. the current transaction.
+     * \warning This is a private field. Don't access it in module code.
+     */
+    unsigned char status;
+};
+
+PICOTM_NOTHROW
+/**
+ * Initializes a reader-writer state.
+ *
+ * \param self  The reader-writer state to initialize.
+ */
+void
+picotm_rwstate_init(struct picotm_rwstate* self);
+
+PICOTM_NOTHROW
+/**
+ * Uninitializes a reader-writer state.
+ *
+ * \param self  The reader-writer state to uninitialize.
+ */
+void
+picotm_rwstate_uninit(struct picotm_rwstate* self);
+
+PICOTM_NOTHROW
+/**
+ * Sets a reader/writer state's status.
+ *
+ * \param   self    The reader/writer state.
+ * \param   status  The status to set.
+ * \returns The reader/writer state's status.
+ */
+void
+picotm_rwstate_set_status(struct picotm_rwstate* self,
+                          enum picotm_rwstate_status status);
+
+PICOTM_NOTHROW
+/**
+ * Returns a reader/writer state's current status.
+ *
+ * \param   self    The reader/writer state.
+ * \returns The reader/writer state's status.
+ */
+enum picotm_rwstate_status
+picotm_rwstate_get_status(const struct picotm_rwstate* self);
+
+PICOTM_NOTHROW
+/**
+ * Tries to acquire a reader lock for the state variable. If the lock
+ * could not be acquired, the error parameter will return a conflict.
+ *
+ * \param       self    The reader/writer state.
+ * \param       rwlock  The reader lock to acquire.
+ * \param[out]  error   Returns a error.
+ */
+void
+picotm_rwstate_try_rdlock(struct picotm_rwstate* self,
+                          struct picotm_rwlock* rwlock,
+                          struct picotm_error* error);
+
+PICOTM_NOTHROW
+/**
+ * Tries to acquire a writer lock for the state variable or upgrade
+ * an acquired reader lock to a writer lock. If the lock could not be
+ * acquired, the error parameter will return a conflict.
+ *
+ * \param       self    The reader/writer state.
+ * \param       rwlock  The reader lock to acquire.
+ * \param[out]  error   Returns a error.
+ */
+void
+picotm_rwstate_try_wrlock(struct picotm_rwstate* self,
+                          struct picotm_rwlock* rwlock,
+                          struct picotm_error* error);
+
+PICOTM_NOTHROW
+/**
+ * Releases a reader-writer lock.
+ *
+ * \param   self    The reader/writer state.
+ * \param   rwlock  The reader/writer lock to release.
+ */
+void
+picotm_rwstate_unlock(struct picotm_rwstate* self,
+                      struct picotm_rwlock* rwlock);
+
+PICOTM_END_DECLS
