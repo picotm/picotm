e6db7740a643db943a8c2d542db24879af4db987
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-07-24 13:32:01 +0200

    libc: Add an open file description and global table

---
+++ lib/modules/libc/src/fd/ofd.c
@@ -0,0 +1,288 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "ofd.h"
+#include <assert.h>
+#include <errno.h>
+#include <picotm/picotm-error.h>
+#include <picotm/picotm-lib-array.h>
+#include <picotm/picotm-lib-rwstate.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void
+init_rwlocks(struct picotm_rwlock* beg, const struct picotm_rwlock* end)
+{
+    while (beg < end) {
+        picotm_rwlock_init(beg);
+        ++beg;
+    }
+}
+
+static void
+uninit_rwlocks(struct picotm_rwlock* beg, const struct picotm_rwlock* end)
+{
+    while (beg < end) {
+        picotm_rwlock_uninit(beg);
+        ++beg;
+    }
+}
+
+void
+ofd_init(struct ofd* self, struct picotm_error* error)
+{
+    assert(self);
+
+    int err = pthread_rwlock_init(&self->lock, NULL);
+    if (err) {
+        picotm_error_set_errno(error, err);
+        return;
+    }
+
+    picotm_ref_init(&self->ref, 0);
+    ofd_id_init(&self->id);
+
+    init_rwlocks(picotm_arraybeg(self->rwlock),
+                 picotm_arrayend(self->rwlock));
+
+    self->offset = 0;
+}
+
+void
+ofd_uninit(struct ofd* self)
+{
+    uninit_rwlocks(picotm_arraybeg(self->rwlock),
+                   picotm_arrayend(self->rwlock));
+
+    ofd_id_uninit(&self->id);
+
+    pthread_rwlock_destroy(&self->lock);
+}
+
+static void
+ofd_rdlock(struct ofd* self)
+{
+    assert(self);
+
+    int err = pthread_rwlock_rdlock(&self->lock);
+    if (err) {
+        abort();
+    }
+}
+
+static void
+ofd_wrlock(struct ofd* self)
+{
+    assert(self);
+
+    int err = pthread_rwlock_wrlock(&self->lock);
+    if (err) {
+        abort();
+    }
+}
+
+static void
+ofd_unlock(struct ofd* self)
+{
+    assert(self);
+
+    int err = pthread_rwlock_unlock(&self->lock);
+    if (err) {
+        abort();
+    }
+}
+
+off_t
+ofd_get_offset(struct ofd* self)
+{
+    assert(self);
+
+    ofd_rdlock(self);
+    off_t offset = self->offset;
+    ofd_unlock(self);
+
+    return offset;
+}
+
+/*
+ * Referencing
+ */
+
+/* requires internal writer lock */
+static void
+ref_or_set_up(struct ofd* self, int fildes, struct picotm_error* error)
+{
+    assert(self);
+
+    bool first_ref = picotm_ref_up(&self->ref);
+    if (!first_ref) {
+        /* we got a set-up instance; signal success */
+        return;
+    }
+
+    ofd_id_set_from_fildes(&self->id, fildes, error);
+    if (picotm_error_is_set(error)) {
+        goto err_ofdid_init_from_fildes;
+    }
+
+    off_t res = lseek(fildes, 0, SEEK_CUR);
+    if (res != (off_t)-1) {
+        /* seekable file; store offset */
+        self->offset = res;
+    } else if (errno != ESPIPE) {
+        /* error other than 'not seekable' */
+        picotm_error_set_errno(error, errno);
+        goto err_lseek;
+    }
+
+    return;
+
+err_lseek:
+err_ofdid_init_from_fildes:
+    ofd_unref(self);
+}
+
+void
+ofd_ref_or_set_up(struct ofd* self, int fildes, struct picotm_error* error)
+{
+    ofd_wrlock(self);
+
+    ref_or_set_up(self, fildes, error);
+    if (picotm_error_is_set(error)) {
+        goto err_ref_or_set_up;
+    }
+
+    ofd_unlock(self);
+
+    return;
+
+err_ref_or_set_up:
+    ofd_unlock(self);
+}
+
+void
+ofd_ref(struct ofd* self)
+{
+    picotm_ref_up(&self->ref);
+}
+
+void
+ofd_unref(struct ofd* self)
+{
+    assert(self);
+
+    ofd_wrlock(self);
+
+    bool final_ref = picotm_ref_down(&self->ref);
+    if (!final_ref) {
+        goto unlock;
+    }
+
+    /* We clear the file on releasing the final reference. This
+     * instance remains initialized, but is available for later
+     * use. */
+    ofd_id_clear(&self->id);
+
+unlock:
+    ofd_unlock(self);
+}
+
+int
+ofd_cmp_and_ref_or_set_up(struct ofd* self, const struct ofd_id* id,
+                          int fildes, struct picotm_error* error)
+{
+    assert(self);
+
+    ofd_wrlock(self);
+
+    int cmp = ofd_id_cmp(&self->id, id);
+    if (cmp) {
+        goto unlock; /* ids are not equal; only return */
+    }
+
+    ref_or_set_up(self, fildes, error);
+    if (picotm_error_is_set(error)) {
+        goto err_ref_or_set_up;
+    }
+
+unlock:
+    ofd_unlock(self);
+
+    return cmp;
+
+err_ref_or_set_up:
+    ofd_unlock(self);
+    return cmp;
+}
+
+int
+ofd_cmp_and_ref(struct ofd* self, const struct ofd_id* id)
+{
+    assert(self);
+
+    ofd_rdlock(self);
+
+    int cmp = ofd_id_cmp(&self->id, id);
+    if (!cmp) {
+        ofd_ref(self);
+    }
+
+    ofd_unlock(self);
+
+    return cmp;
+}
+
+/*
+ * Locking
+ */
+
+void
+ofd_try_rdlock_field(struct ofd* self, enum ofd_field field,
+                     struct picotm_rwstate* rwstate,
+                     struct picotm_error* error)
+{
+    assert(self);
+
+    picotm_rwstate_try_rdlock(rwstate, self->rwlock + field, error);
+    if (picotm_error_is_set(error)) {
+        return;
+    }
+}
+
+void
+ofd_try_wrlock_field(struct ofd* self, enum ofd_field field,
+                     struct picotm_rwstate* rwstate,
+                     struct picotm_error* error)
+{
+    assert(self);
+
+    picotm_rwstate_try_wrlock(rwstate, self->rwlock + field, error);
+    if (picotm_error_is_set(error)) {
+        return;
+    }
+}
+
+void
+ofd_unlock_field(struct ofd* self, enum ofd_field field,
+                 struct picotm_rwstate* rwstate)
+{
+    assert(self);
+
+    picotm_rwstate_unlock(rwstate, self->rwlock + field);
+}
---
+++ lib/modules/libc/src/fd/ofd.h
@@ -0,0 +1,175 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <picotm/picotm-lib-ref.h>
+#include <picotm/picotm-lib-rwlock.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include "ofd_id.h"
+
+/**
+ * \cond impl || libc_impl || libc_impl_fd
+ * \ingroup libc_impl
+ * \ingroup libc_impl_fd
+ * \file
+ * \endcond
+ */
+
+struct picotm_rwstate;
+
+/**
+ * Enumerates fields of `struct ofd`.
+ */
+enum ofd_field {
+    OFD_FIELD_FILE_OFFSET,
+    OFD_FIELD_STATE,
+    NUMBER_OF_OFD_FIELDS
+};
+
+/**
+ * Represents an open file description.
+ */
+struct ofd {
+
+    /** Internal lock. */
+    pthread_rwlock_t lock;
+
+    /** The reference counter. */
+    struct picotm_shared_ref16 ref;
+
+    /** The open file description's unique id. */
+    struct ofd_id id;
+
+    /** Reader/writer state locks. */
+    struct picotm_rwlock  rwlock[NUMBER_OF_OFD_FIELDS];
+
+    /** \brief Current file offset. */
+    off_t offset;
+};
+
+/**
+ * \brief Initializes an open file description.
+ * \param       self    The open file description to initialize.
+ * \param[out]  error   Returns an error to the caller.
+ */
+void
+ofd_init(struct ofd* self, struct picotm_error* error);
+
+/**
+ * \brief Uninitializes an open file description.
+ * \param   self    The open file description to uninitialize.
+ */
+void
+ofd_uninit(struct ofd* self);
+
+/**
+ * \brief Returns the current file offset of a open file description.
+ * \param   self    The open file description.
+ * \returns The open file description's current file offset.
+ */
+off_t
+ofd_get_offset(struct ofd* self);
+
+/**
+ * \brief Acquires a reference on an open file description.
+ * \param   self    The open file description.
+ */
+void
+ofd_ref(struct ofd* self);
+
+/**
+ * \brief Sets up an open file description or acquires a reference
+ *        on an already set-up instance.
+ * \param       self    The open file description.
+ * \param       fildes  A file descriptor for the setup.
+ * \param[out]  error   Returns an error to the caller.
+ */
+void
+ofd_ref_or_set_up(struct ofd* self, int fildes,
+                  struct picotm_error* error);
+
+/**
+ * \brief Releases a reference on an open file description.
+ * \param   self    The open file description.
+ */
+void
+ofd_unref(struct ofd* self);
+
+/**
+ * \brief Compares the open file description's id to a reference id and
+ *        acquires a reference if both id's are equal. The instance is
+ *        set up from the provided file descriptor if necessary.
+ * \param       self        The open file description.
+ * \param       id          The id to compare to.
+ * \param       fildes      A file descriptor for the setup.
+ * \param[out]  error       Returns an error ot the caller.
+ * \returns A value less than, equal to, or greater than if the ofd's id is
+ *          less than, equal to, or greater than the given id.
+ */
+int
+ofd_cmp_and_ref_or_set_up(struct ofd* self, const struct ofd_id* id,
+                          int fildes, struct picotm_error* error);
+
+/**
+ * \brief Compares the open file description's id to a reference id and
+ *        acquires a reference if both id's are equal.
+ * \param   self    The open file description.
+ * \param   id      The id to compare to.
+ * \returns A value less than, equal to, or greater than if the ofd's id is
+ *          less than, equal to, or greater than the given id.
+ */
+int
+ofd_cmp_and_ref(struct ofd* self, const struct ofd_id* id);
+
+/**
+ * \brief Tries to acquire a reader lock on an open file description.
+ * \param       self        The open file description.
+ * \param       field       The reader lock's field.
+ * \param       rwstate     The transaction's reader/writer state.
+ * \param[out]  error       Returns an error ot the caller.
+ */
+void
+ofd_try_rdlock_field(struct ofd* self, enum ofd_field field,
+                     struct picotm_rwstate* rwstate,
+                     struct picotm_error* error);
+
+/**
+ * \brief Tries to acquire a writer lock on an open file description.
+ * \param       self        The open file description.
+ * \param       field       The writer lock's field.
+ * \param       rwstate     The transaction's reader/writer state.
+ * \param[out]  error       Returns an error ot the caller.
+ */
+void
+ofd_try_wrlock_field(struct ofd* self, enum ofd_field field,
+                     struct picotm_rwstate* rwstate,
+                     struct picotm_error* error);
+
+/**
+ * \brief Releases a lock on an open file description.
+ * \param   self    The open file description.
+ * \param   field   The reader/writer lock's field.
+ * \param   rwstate The transaction's reader/writer state.
+ */
+void
+ofd_unlock_field(struct ofd* self, enum ofd_field field,
+                 struct picotm_rwstate* rwstate);
