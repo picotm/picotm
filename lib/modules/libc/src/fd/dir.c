1e631e54f9201d8af3930b7dd598e979785a75c5
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-08-20 14:30:04 +0200

    libc: Implement `struct dir` with `picotm_shared_ref16_obj`

--- lib/modules/libc/src/fd/dir.c
+++ lib/modules/libc/src/fd/dir.c
@@ -21,8 +21,15 @@
 #include <assert.h>
 #include <picotm/picotm-error.h>
 #include <picotm/picotm-lib-array.h>
+#include <picotm/picotm-lib-ptr.h>
 #include <picotm/picotm-lib-rwstate.h>
 #include <stdlib.h>
+
+static struct dir*
+dir_of_picotm_shared_ref16_obj(struct picotm_shared_ref16_obj* ref_obj)
+{
+    return picotm_containerof(ref_obj, struct dir, ref_obj);
+}
 
 static void
 init_rwlocks(struct picotm_rwlock* beg, const struct picotm_rwlock* end)
@@ -47,13 +54,11 @@ dir_init(struct dir* self, struct picotm_error* error)
 {
     assert(self);
 
-    int err = pthread_rwlock_init(&self->lock, NULL);
-    if (err) {
-        picotm_error_set_errno(error, err);
+    picotm_shared_ref16_obj_init(&self->ref_obj, error);
+    if (picotm_error_is_set(error)) {
         return;
     }
 
-    picotm_ref_init(&self->ref, 0);
     file_id_clear(&self->id);
 
     init_rwlocks(picotm_arraybeg(self->rwlock),
@@ -63,41 +68,15 @@ dir_init(struct dir* self, struct picotm_error* error)
 void
 dir_uninit(struct dir* self)
 {
+    assert(self);
+
     uninit_rwlocks(picotm_arraybeg(self->rwlock),
                    picotm_arrayend(self->rwlock));
 
-    pthread_rwlock_destroy(&self->lock);
-}
+    struct picotm_error error = PICOTM_ERROR_INITIALIZER;
 
-static void
-dir_rdlock(struct dir* self)
-{
-    assert(self);
-
-    int err = pthread_rwlock_rdlock(&self->lock);
-    if (err) {
-        abort();
-    }
-}
-
-static void
-dir_wrlock(struct dir* self)
-{
-    assert(self);
-
-    int err = pthread_rwlock_wrlock(&self->lock);
-    if (err) {
-        abort();
-    }
-}
-
-static void
-dir_unlock(struct dir* self)
-{
-    assert(self);
-
-    int err = pthread_rwlock_unlock(&self->lock);
-    if (err) {
+    picotm_shared_ref16_obj_uninit(&self->ref_obj, &error);
+    if (picotm_error_is_set(&error)) {
         abort();
     }
 }
@@ -106,52 +85,70 @@ dir_unlock(struct dir* self)
  * Referencing
  */
 
-/* requires internal writer lock */
+struct ref_obj_data {
+    const struct file_id* id;
+    int fildes;
+    int cmp;
+};
+
 static void
-ref_or_set_up(struct dir* self, int fildes, struct picotm_error* error)
+first_ref(struct picotm_shared_ref16_obj* ref_obj, void* data,
+          struct picotm_error* error)
 {
+    struct dir* self = dir_of_picotm_shared_ref16_obj(ref_obj);
     assert(self);
 
-    bool first_ref = picotm_ref_up(&self->ref);
-    if (!first_ref) {
-        /* we got a set-up instance; signal success */
+    const struct ref_obj_data* ref_obj_data = data;
+    assert(ref_obj_data);
+
+    file_id_init_from_fildes(&self->id, ref_obj_data->fildes, error);
+    if (picotm_error_is_set(error)) {
         return;
     }
-
-    file_id_init_from_fildes(&self->id, fildes, error);
-    if (picotm_error_is_set(error)) {
-        goto err_file_id_init_from_fildes;
-    }
-
-    return;
-
-err_file_id_init_from_fildes:
-    dir_unref(self);
 }
 
 void
-dir_ref_or_set_up(struct dir* self, int fildes,
-                     struct picotm_error* error)
+dir_ref_or_set_up(struct dir* self, int fildes, struct picotm_error* error)
 {
-    dir_wrlock(self);
+    assert(self);
 
-    ref_or_set_up(self, fildes, error);
+    struct ref_obj_data data = {
+        NULL,
+        fildes,
+        0
+    };
+
+    picotm_shared_ref16_obj_up(&self->ref_obj, &data, NULL, first_ref,
+                               error);
     if (picotm_error_is_set(error)) {
-        goto err_ref_or_set_up;
+        return;
     }
-
-    dir_unlock(self);
-
-    return;
-
-err_ref_or_set_up:
-    dir_unlock(self);
 }
 
 void
 dir_ref(struct dir* self)
 {
-    picotm_ref_up(&self->ref);
+    assert(self);
+
+    struct picotm_error error = PICOTM_ERROR_INITIALIZER;
+
+    picotm_shared_ref16_obj_up(&self->ref_obj, NULL, NULL, NULL, &error);
+    if (picotm_error_is_set(&error)) {
+        abort();
+    }
+}
+
+static void
+final_ref(struct picotm_shared_ref16_obj* ref_obj, void* data,
+          struct picotm_error* error)
+{
+    struct dir* self = dir_of_picotm_shared_ref16_obj(ref_obj);
+    assert(self);
+
+    /* We clear the id on releasing the final reference. This
+     * instance remains initialized, but is available for later
+     * use. */
+    file_id_clear(&self->id);
 }
 
 void
@@ -159,20 +156,28 @@ dir_unref(struct dir* self)
 {
     assert(self);
 
-    dir_wrlock(self);
+    struct picotm_error error = PICOTM_ERROR_INITIALIZER;
 
-    bool final_ref = picotm_ref_down(&self->ref);
-    if (!final_ref) {
-        goto unlock;
+    picotm_shared_ref16_obj_down(&self->ref_obj, NULL, NULL, final_ref,
+                                 &error);
+    if (picotm_error_is_set(&error)) {
+        abort();
     }
+}
 
-    /* We clear the id on releasing the final reference. This
-     * instance remains initialized, but is available for later
-     * use. */
-    file_id_clear(&self->id);
+static bool
+cond_ref(struct picotm_shared_ref16_obj* ref_obj, void* data,
+         struct picotm_error* error)
+{
+    struct dir* self = dir_of_picotm_shared_ref16_obj(ref_obj);
+    assert(self);
 
-unlock:
-    dir_unlock(self);
+    struct ref_obj_data* ref_obj_data = data;
+    assert(ref_obj_data);
+
+    ref_obj_data->cmp = file_id_cmp(&self->id, ref_obj_data->id);
+
+    return !ref_obj_data->cmp;
 }
 
 int
@@ -181,26 +186,19 @@ dir_cmp_and_ref_or_set_up(struct dir* self, const struct file_id* id,
 {
     assert(self);
 
-    dir_wrlock(self);
+    struct ref_obj_data data = {
+        id,
+        fildes,
+        0
+    };
 
-    int cmp = file_id_cmp(&self->id, id);
-    if (cmp) {
-        goto unlock; /* ids are not equal; only return */
-    }
-
-    ref_or_set_up(self, fildes, error);
+    picotm_shared_ref16_obj_up(&self->ref_obj, &data, cond_ref, first_ref,
+                               error);
     if (picotm_error_is_set(error)) {
-        goto err_ref_or_set_up;
+        return 0;
     }
 
-unlock:
-    dir_unlock(self);
-
-    return cmp;
-
-err_ref_or_set_up:
-    dir_unlock(self);
-    return cmp;
+    return data.cmp;
 }
 
 int
@@ -208,16 +206,20 @@ dir_cmp_and_ref(struct dir* self, const struct file_id* id)
 {
     assert(self);
 
-    dir_rdlock(self);
+    struct ref_obj_data data = {
+        id,
+        -1,
+        0
+    };
 
-    int cmp = file_id_cmp(&self->id, id);
-    if (!cmp) {
-        dir_ref(self);
+    struct picotm_error error = PICOTM_ERROR_INITIALIZER;
+
+    picotm_shared_ref16_obj_up(&self->ref_obj, &data, cond_ref, NULL, &error);
+    if (picotm_error_is_set(&error)) {
+        return 0;
     }
 
-    dir_unlock(self);
-
-    return cmp;
+    return data.cmp;
 }
 
 void
