5e40b92133999249377d2e1fc9b18815aca6686c
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-08-30 14:52:02 +0200

    tests: Add TAP protocol primitives and utility functions

---
+++ tests/src/taputils.c
@@ -0,0 +1,96 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "taputils.h"
+#include <string.h>
+#include "tap.h"
+
+/*
+ * TAP output with different levels of verbosity
+ */
+
+enum tap_verbosity g_tap_verbosity = TAP_ERROR;
+
+void
+tap_error_va(const char* msg, va_list ap)
+{
+    if (g_tap_verbosity < TAP_ERROR) {
+        return;
+    }
+    tap_diag_va(msg, ap);
+}
+
+void
+tap_info_va(const char* msg, va_list ap)
+{
+    if (g_tap_verbosity < TAP_INFO) {
+        return;
+    }
+    tap_diag_va(msg, ap);
+}
+
+void
+tap_debug_va(const char* msg, va_list ap)
+{
+    if (g_tap_verbosity < TAP_DEBUG) {
+        return;
+    }
+    tap_diag_va(msg, ap);
+}
+
+void
+tap_error(const char* msg, ...)
+{
+    va_list ap;
+    va_start(ap, msg);
+    tap_error_va(msg, ap);
+    va_end(ap);
+}
+
+void
+tap_info(const char* msg, ...)
+{
+    va_list ap;
+    va_start(ap, msg);
+    tap_info_va(msg, ap);
+    va_end(ap);
+}
+
+void
+tap_debug(const char* msg, ...)
+{
+    va_list ap;
+    va_start(ap, msg);
+    tap_debug_va(msg, ap);
+    va_end(ap);
+}
+
+/*
+ * Error reporting
+ */
+
+void
+tap_error_errno_at(const char* function, int errnum, const char* filename,
+                   unsigned long line)
+{
+    char buf[100];
+
+    tap_error("%s:%lu: %s failed: %d (%s)", filename, line, function,
+              errnum, strerror_r(errnum, buf, sizeof(buf)));
+}
