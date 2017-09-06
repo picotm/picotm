2de0a6374d12fc4c42966671a45a73a1b20399f4
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-09-06 11:16:52 +0200

    libc: Add transaction-safe implementation of vsscanf()

---
+++ lib/modules/libc/src/stdio-tm.c
@@ -0,0 +1,39 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "picotm/stdio-tm.h"
+#include <errno.h>
+#include <picotm/picotm-module.h>
+#include "error/module.h"
+
+PICOTM_EXPORT
+int
+vsscanf_tm(const char* s, const char* format, va_list arg)
+{
+    error_module_save_errno();
+
+    do {
+        int res = vsscanf(s, format, arg);
+        if (res < 0) {
+            picotm_recover_from_errno(errno);
+            continue;
+        }
+        return res;
+    } while (true);
+}
