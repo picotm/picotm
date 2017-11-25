d857b45e4e691aed5abc292d67fb2b0983baa8ca
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-11-25 18:29:00 +0100

    tests: Replace safe_pthread_yield() with safe_sched_yield()
    The function sched_yield() is prefered over pthread_yield() as
    it's standardized by POSIX. This patch replaces all occurences
    of safe_pthread_yield() and removes the function.

--- tests/libtests/safe_sched.h
+++ tests/libtests/safe_sched.h
@@ -19,28 +19,7 @@
 
 #pragma once
 
-#include <pthread.h>
+#include <sched.h>
 
 int
-safe_pthread_barrier_destroy(pthread_barrier_t* barrier);
-
-int
-safe_pthread_barrier_init(pthread_barrier_t* restrict barrier,
-                          const pthread_barrierattr_t* restrict attr,
-                          unsigned count);
-
-int
-safe_pthread_barrier_wait(pthread_barrier_t* barrier);
-
-int
-safe_pthread_create(pthread_t* thread, const pthread_attr_t* attr,
-                    void* (*start_routine)(void*), void* arg);
-
-int
-safe_pthread_join(pthread_t thread, void** retval);
-
-int
-safe_pthread_mutex_lock(pthread_mutex_t* mutex);
-
-int
-safe_pthread_mutex_unlock(pthread_mutex_t* mutex);
+safe_sched_yield(void);
