2a30feacc15f599b5b97c373494b23562e120f84
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-12-08 13:43:22 +0100

    libc: Provide wrapper around system's variant of malloc_usable_size()

---
+++ modules/libc/src/compat/malloc_usable_size.c
@@ -0,0 +1,35 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "malloc_usable_size.h"
+#if defined(HAVE_MALLOC_MALLOC_H) && HAVE_MALLOC_MALLOC_H
+#include <malloc/malloc.h>
+#endif
+
+size_t
+__picotm_libc_malloc_usable_size(void* ptr)
+{
+#if (!defined(HAVE_DECL_MALLOC_USABLE_SIZE) || \
+        !HAVE_DECL_MALLOC_USABLE_SIZE) && \
+    (defined(HAVE_DECL_MALLOC_SIZE) && HAVE_DECL_MALLOC_SIZE)
+    return malloc_size(ptr);
+#else
+    return 0;
+#endif
+}
