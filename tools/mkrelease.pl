91a13304cb3b36a00866c93790e4b9c3c10684bc
Thomas Zimmermann <tdz@users.sourceforge.net>
2017-12-16 11:17:29 +0100

    Add SPDX license identifier to all MIT-licensed files

--- tools/mkrelease.pl
+++ tools/mkrelease.pl
@@ -21,6 +21,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
 
 # Run this script in the repository's top-level directory to generate
 # a new release.
