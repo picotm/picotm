3194edf5d9004b704c2d23b2537443f29da90efc
Thomas Zimmermann <tdz@users.sourceforge.net>
2018-01-17 11:07:28 +0100

    core: Add out-of-bounds memory-access error code

--- include/picotm/picotm-error.h
+++ include/picotm/picotm-error.h
@@ -1,6 +1,6 @@
 /*
  * MIT License
- * Copyright (c) 2017   Thomas Zimmermann <tdz@users.sourceforge.net>
+ * Copyright (c) 2017-2018  Thomas Zimmermann <tdz@users.sourceforge.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -59,7 +59,9 @@ enum picotm_error_code {
     /** Out-of-Memory error. */
     PICOTM_OUT_OF_MEMORY,
     /** Invalid floating-point environment. */
-    PICOTM_INVALID_FENV
+    PICOTM_INVALID_FENV,
+    /** Out-of-Bounds memory access. */
+    PICOTM_OUT_OF_BOUNDS
 };
 
 /**
