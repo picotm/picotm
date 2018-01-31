9e7a5b684a2bb34ad4b631058bcc9d4d4264db10
Thomas Zimmermann <tdz@users.sourceforge.net>
2018-01-31 23:32:21 +0100

    tm: Clean up block and frame constants

--- modules/tm/src/block.h
+++ modules/tm/src/block.h
@@ -42,9 +42,10 @@
  * aligment and minimal size of malloc'ed memory. Otherwise we might
  * corrupt malloc's internal data structures.
  */
-#define TM_BLOCK_SIZE_BITS  (3)
-#define TM_BLOCK_SIZE       (1ul << TM_BLOCK_SIZE_BITS)
-#define TM_BLOCK_MASK       (~(TM_BLOCK_SIZE - 1))
+#define TM_BLOCK_SIZE_BITS      (3)
+#define TM_BLOCK_SIZE           (1ul << TM_BLOCK_SIZE_BITS)
+#define TM_BLOCK_OFFSET_MASK    ((1ul << TM_BLOCK_SIZE) - 1)
+#define TM_BLOCK_INDEX_MASK     (~TM_BLOCK_FIELD_MASK)
 
 static inline size_t
 tm_block_index_at(uintptr_t addr)
@@ -55,5 +56,5 @@ tm_block_index_at(uintptr_t addr)
 static inline uintptr_t
 tm_block_offset_at(uintptr_t addr)
 {
-    return addr & TM_BLOCK_MASK;
+    return addr & TM_BLOCK_OFFSET_MASK;
 }
