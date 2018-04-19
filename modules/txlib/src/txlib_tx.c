eaf2be0d8732f233d0d256dd296e4ab5b5e6e089
Thomas Zimmermann <tdz@users.sourceforge.net>
2018-04-19 15:06:47 +0200

    txlib: Convert txlib data stuctures to |struct picotm_slist|

--- modules/txlib/src/txlib_tx.c
+++ modules/txlib/src/txlib_tx.c
@@ -31,7 +31,6 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 #include "txlib_event.h"
 
 void
@@ -41,11 +40,11 @@ txlib_tx_init(struct txlib_tx* self, unsigned long module)
 
     self->module = module;
 
-    SLIST_INIT(&self->allocated_entries);
-    SLIST_INIT(&self->acquired_list_tx);
-    SLIST_INIT(&self->acquired_multiset_tx);
-    SLIST_INIT(&self->acquired_queue_tx);
-    SLIST_INIT(&self->acquired_stack_tx);
+    picotm_slist_init_head(&self->allocated_entries);
+    picotm_slist_init_head(&self->acquired_list_tx);
+    picotm_slist_init_head(&self->acquired_multiset_tx);
+    picotm_slist_init_head(&self->acquired_queue_tx);
+    picotm_slist_init_head(&self->acquired_stack_tx);
 
     self->event = NULL;
     self->nevents = 0;
@@ -54,10 +53,11 @@ txlib_tx_init(struct txlib_tx* self, unsigned long module)
 static void
 free_allocated_txlib_tx_entries(struct txlib_tx* self)
 {
-    struct txlib_tx_entry* entry = SLIST_FIRST(&self->allocated_entries);
-
-    for (; entry; entry = SLIST_FIRST(&self->allocated_entries)) {
-        SLIST_REMOVE_HEAD(&self->allocated_entries, slist_entry);
+    while (!picotm_slist_is_empty(&self->allocated_entries)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->allocated_entries));
+        picotm_slist_dequeue_front(&self->allocated_entries);
+        picotm_slist_uninit_item(&entry->slist_entry);
         free(entry);
     }
 }
@@ -69,10 +69,16 @@ txlib_tx_uninit(struct txlib_tx* self)
 
     free_allocated_txlib_tx_entries(self);
 
-    assert(SLIST_EMPTY(&self->acquired_list_tx));
-    assert(SLIST_EMPTY(&self->acquired_multiset_tx));
-    assert(SLIST_EMPTY(&self->acquired_queue_tx));
-    assert(SLIST_EMPTY(&self->acquired_stack_tx));
+    assert(picotm_slist_is_empty(&self->acquired_list_tx));
+    assert(picotm_slist_is_empty(&self->acquired_multiset_tx));
+    assert(picotm_slist_is_empty(&self->acquired_queue_tx));
+    assert(picotm_slist_is_empty(&self->acquired_stack_tx));
+
+    picotm_slist_uninit_head(&self->allocated_entries);
+    picotm_slist_uninit_head(&self->acquired_list_tx);
+    picotm_slist_uninit_head(&self->acquired_multiset_tx);
+    picotm_slist_uninit_head(&self->acquired_queue_tx);
+    picotm_slist_uninit_head(&self->acquired_stack_tx);
 
     picotm_tabfree(self->event);
 }
@@ -82,22 +88,34 @@ allocate_txlib_tx_entry(struct txlib_tx* self, struct picotm_error* error)
 {
     struct txlib_tx_entry* entry;
 
-    if (SLIST_EMPTY(&self->allocated_entries)) {
+    if (picotm_slist_is_empty(&self->allocated_entries)) {
 
         entry = malloc(sizeof(*entry));
         if (!entry) {
             picotm_error_set_errno(error, errno);
             return NULL;
         }
-        memset(&entry->slist_entry, 0, sizeof(entry->slist_entry));
+        picotm_slist_init_item(&entry->slist_entry);
 
     } else {
-
-        entry = SLIST_FIRST(&self->allocated_entries);
-        SLIST_REMOVE_HEAD(&self->allocated_entries, slist_entry);
+        entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->allocated_entries));
+        picotm_slist_dequeue_front(&self->allocated_entries);
     }
 
     return entry;
+}
+
+static _Bool
+has_txlist_state(const struct txlib_tx_entry* entry, struct txlist_state* list_state)
+{
+    return entry->data.list_tx.list_state == list_state;
+}
+
+static _Bool
+has_txlist_state_cb(const struct picotm_slist* item, void* list_state)
+{
+    return has_txlist_state(txlib_tx_entry_of_slist_const(item), list_state);
 }
 
 struct txlist_tx*
@@ -107,23 +125,37 @@ txlib_tx_acquire_txlist_of_state(struct txlib_tx* self,
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
-
-    SLIST_FOREACH(entry, &self->acquired_list_tx, slist_entry) {
-        if (entry->data.list_tx.list_state == list_state) {
-            return &entry->data.list_tx;
-        }
+    struct picotm_slist* item = picotm_slist_find1(&self->acquired_list_tx,
+                                                   has_txlist_state_cb,
+                                                   list_state);
+    if (item != picotm_slist_end(&self->acquired_list_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(item);
+        return &entry->data.list_tx;
     }
 
-    entry = allocate_txlib_tx_entry(self, error);
+    struct txlib_tx_entry* entry = allocate_txlib_tx_entry(self, error);
     if (picotm_error_is_set(error)) {
         return NULL;
     }
 
     txlist_tx_init(&entry->data.list_tx, list_state, self);
-    SLIST_INSERT_HEAD(&self->acquired_list_tx, entry, slist_entry);
+    picotm_slist_enqueue_front(&self->acquired_list_tx, &entry->slist_entry);
 
     return &entry->data.list_tx;
+}
+
+static _Bool
+has_txmultiset_state(const struct txlib_tx_entry* entry,
+                           struct txmultiset_state* multiset_state)
+{
+    return entry->data.multiset_tx.multiset_state == multiset_state;
+}
+
+static _Bool
+has_txmultiset_state_cb(const struct picotm_slist* item, void* multiset_state)
+{
+    return has_txmultiset_state(txlib_tx_entry_of_slist_const(item),
+                                multiset_state);
 }
 
 struct txmultiset_tx*
@@ -133,23 +165,38 @@ txlib_tx_acquire_txmultiset_of_state(struct txlib_tx* self,
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
-
-    SLIST_FOREACH(entry, &self->acquired_multiset_tx, slist_entry) {
-        if (entry->data.multiset_tx.multiset_state == multiset_state) {
-            return &entry->data.multiset_tx;
-        }
+    struct picotm_slist* item = picotm_slist_find1(&self->acquired_multiset_tx,
+                                                   has_txmultiset_state_cb,
+                                                   multiset_state);
+    if (item != picotm_slist_end(&self->acquired_multiset_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(item);
+        return &entry->data.multiset_tx;
     }
 
-    entry = allocate_txlib_tx_entry(self, error);
+    struct txlib_tx_entry* entry = allocate_txlib_tx_entry(self, error);
     if (picotm_error_is_set(error)) {
         return NULL;
     }
 
     txmultiset_tx_init(&entry->data.multiset_tx, multiset_state, self);
-    SLIST_INSERT_HEAD(&self->acquired_multiset_tx, entry, slist_entry);
+    picotm_slist_enqueue_front(&self->acquired_multiset_tx,
+                               &entry->slist_entry);
 
     return &entry->data.multiset_tx;
+}
+
+static _Bool
+has_txqueue_state(const struct txlib_tx_entry* entry,
+                        struct txqueue_state* queue_state)
+{
+    return entry->data.queue_tx.queue_state == queue_state;
+}
+
+static _Bool
+has_txqueue_state_cb(const struct picotm_slist* item, void* queue_state)
+{
+    return has_txqueue_state(txlib_tx_entry_of_slist_const(item),
+                             queue_state);
 }
 
 struct txqueue_tx*
@@ -159,23 +206,37 @@ txlib_tx_acquire_txqueue_of_state(struct txlib_tx* self,
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
-
-    SLIST_FOREACH(entry, &self->acquired_queue_tx, slist_entry) {
-        if (entry->data.queue_tx.queue_state == queue_state) {
-            return &entry->data.queue_tx;
-        }
+    struct picotm_slist* item = picotm_slist_find1(&self->acquired_queue_tx,
+                                                   has_txqueue_state_cb,
+                                                   queue_state);
+    if (item != picotm_slist_end(&self->acquired_queue_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(item);
+        return &entry->data.queue_tx;
     }
 
-    entry = allocate_txlib_tx_entry(self, error);
+    struct txlib_tx_entry* entry = allocate_txlib_tx_entry(self, error);
     if (picotm_error_is_set(error)) {
         return NULL;
     }
 
     txqueue_tx_init(&entry->data.queue_tx, queue_state, self);
-    SLIST_INSERT_HEAD(&self->acquired_queue_tx, entry, slist_entry);
+    picotm_slist_enqueue_front(&self->acquired_queue_tx, &entry->slist_entry);
 
     return &entry->data.queue_tx;
+}
+
+static _Bool
+has_txstack_state(const struct txlib_tx_entry* entry,
+                        struct txstack_state* stack_state)
+{
+    return entry->data.stack_tx.stack_state == stack_state;
+}
+
+static _Bool
+has_txstack_state_cb(const struct picotm_slist* item, void* stack_state)
+{
+    return has_txstack_state(txlib_tx_entry_of_slist_const(item),
+                             stack_state);
 }
 
 struct txstack_tx*
@@ -185,21 +246,21 @@ txlib_tx_acquire_txstack_of_state(struct txlib_tx* self,
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
-
-    SLIST_FOREACH(entry, &self->acquired_stack_tx, slist_entry) {
-        if (entry->data.stack_tx.stack_state == stack_state) {
-            return &entry->data.stack_tx;
-        }
+    struct picotm_slist* item = picotm_slist_find1(&self->acquired_stack_tx,
+                                                   has_txstack_state_cb,
+                                                   stack_state);
+    if (item != picotm_slist_end(&self->acquired_stack_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(item);
+        return &entry->data.stack_tx;
     }
 
-    entry = allocate_txlib_tx_entry(self, error);
+    struct txlib_tx_entry* entry = allocate_txlib_tx_entry(self, error);
     if (picotm_error_is_set(error)) {
         return NULL;
     }
 
     txstack_tx_init(&entry->data.stack_tx, stack_state, self);
-    SLIST_INSERT_HEAD(&self->acquired_stack_tx, entry, slist_entry);
+    picotm_slist_enqueue_front(&self->acquired_stack_tx, &entry->slist_entry);
 
     return &entry->data.stack_tx;
 }
@@ -284,19 +345,47 @@ txlib_tx_append_events1(struct txlib_tx* self, size_t nevents,
  * Module interface
  */
 
+static size_t
+lock_txqueue_tx_entry(struct txlib_tx_entry* entry,
+                      struct picotm_error* error)
+{
+    txqueue_tx_lock(&entry->data.queue_tx, error);
+    if (picotm_error_is_set(error)) {
+        return 0;
+    }
+    return 1;
+}
+
+static size_t
+lock_txqueue_tx_entry_cb(struct picotm_slist* item, void* data)
+{
+    return lock_txqueue_tx_entry(txlib_tx_entry_of_slist(item), data);
+}
+
 static void
 lock_txqueue_tx_entries(struct txlib_tx* self, struct picotm_error* error)
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
+    picotm_slist_walk_1(&self->acquired_queue_tx, lock_txqueue_tx_entry_cb,
+                        error);
+}
 
-    SLIST_FOREACH(entry, &self->acquired_queue_tx, slist_entry) {
-        txqueue_tx_lock(&entry->data.queue_tx, error);
-        if (picotm_error_is_set(error)) {
-            return;
-        }
+static size_t
+lock_txstack_tx_entry(struct txlib_tx_entry* entry,
+                      struct picotm_error* error)
+{
+    txstack_tx_lock(&entry->data.stack_tx, error);
+    if (picotm_error_is_set(error)) {
+        return 0;
     }
+    return 1;
+}
+
+static size_t
+lock_txstack_tx_entry_cb(struct picotm_slist* item, void* data)
+{
+    return lock_txstack_tx_entry(txlib_tx_entry_of_slist(item), data);
 }
 
 static void
@@ -304,19 +393,17 @@ lock_txstack_tx_entries(struct txlib_tx* self, struct picotm_error* error)
 {
     assert(self);
 
-    struct txlib_tx_entry* entry;
-
-    SLIST_FOREACH(entry, &self->acquired_stack_tx, slist_entry) {
-        txstack_tx_lock(&entry->data.stack_tx, error);
-        if (picotm_error_is_set(error)) {
-            return;
-        }
-    }
+    picotm_slist_walk_1(&self->acquired_stack_tx, lock_txstack_tx_entry_cb,
+                        error);
 }
 
 void
 txlib_tx_lock(struct txlib_tx* self, struct picotm_error* error)
 {
+    /* Currently, no locking is required for lists and multisets. Both
+     * data structures acquire their locks during the transaction's
+     * execution phase. */
+
     lock_txqueue_tx_entries(self, error);
     if (picotm_error_is_set(error)) {
         return;
@@ -357,56 +444,64 @@ txlib_tx_undo_event(struct txlib_tx* self, unsigned short op,
 static void
 finish_txlist_tx_entries(struct txlib_tx* self)
 {
-    struct txlib_tx_entry* entry = SLIST_FIRST(&self->acquired_list_tx);
+    while (!picotm_slist_is_empty(&self->acquired_list_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->acquired_list_tx));
+        picotm_slist_dequeue_front(&self->acquired_list_tx);
 
-    for (; entry; entry = SLIST_FIRST(&self->acquired_list_tx)) {
         txlist_tx_finish(&entry->data.list_tx);
         txlist_tx_uninit(&entry->data.list_tx);
 
-        SLIST_REMOVE_HEAD(&self->acquired_list_tx, slist_entry);
-        SLIST_INSERT_HEAD(&self->allocated_entries, entry, slist_entry);
+        picotm_slist_enqueue_front(&self->allocated_entries,
+                                   &entry->slist_entry);
     }
 }
 
 static void
 finish_txmultiset_tx_entries(struct txlib_tx* self)
 {
-    struct txlib_tx_entry* entry = SLIST_FIRST(&self->acquired_multiset_tx);
+    while (!picotm_slist_is_empty(&self->acquired_multiset_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->acquired_multiset_tx));
+        picotm_slist_dequeue_front(&self->acquired_multiset_tx);
 
-    for (; entry; entry = SLIST_FIRST(&self->acquired_multiset_tx)) {
         txmultiset_tx_finish(&entry->data.multiset_tx);
         txmultiset_tx_uninit(&entry->data.multiset_tx);
 
-        SLIST_REMOVE_HEAD(&self->acquired_multiset_tx, slist_entry);
-        SLIST_INSERT_HEAD(&self->allocated_entries, entry, slist_entry);
+        picotm_slist_enqueue_front(&self->allocated_entries,
+                                   &entry->slist_entry);
     }
 }
 
 static void
 finish_txqueue_tx_entries(struct txlib_tx* self)
 {
-    struct txlib_tx_entry* entry = SLIST_FIRST(&self->acquired_queue_tx);
+    while (!picotm_slist_is_empty(&self->acquired_queue_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->acquired_queue_tx));
+        picotm_slist_dequeue_front(&self->acquired_queue_tx);
 
-    for (; entry; entry = SLIST_FIRST(&self->acquired_queue_tx)) {
         txqueue_tx_finish(&entry->data.queue_tx);
         txqueue_tx_uninit(&entry->data.queue_tx);
 
-        SLIST_REMOVE_HEAD(&self->acquired_queue_tx, slist_entry);
-        SLIST_INSERT_HEAD(&self->allocated_entries, entry, slist_entry);
+        picotm_slist_enqueue_front(&self->allocated_entries,
+                                   &entry->slist_entry);
     }
 }
 
 static void
 finish_txstack_tx_entries(struct txlib_tx* self)
 {
-    struct txlib_tx_entry* entry = SLIST_FIRST(&self->acquired_stack_tx);
+    while (!picotm_slist_is_empty(&self->acquired_stack_tx)) {
+        struct txlib_tx_entry* entry = txlib_tx_entry_of_slist(
+            picotm_slist_front(&self->acquired_stack_tx));
+        picotm_slist_dequeue_front(&self->acquired_stack_tx);
 
-    for (; entry; entry = SLIST_FIRST(&self->acquired_stack_tx)) {
         txstack_tx_finish(&entry->data.stack_tx);
         txstack_tx_uninit(&entry->data.stack_tx);
 
-        SLIST_REMOVE_HEAD(&self->acquired_stack_tx, slist_entry);
-        SLIST_INSERT_HEAD(&self->allocated_entries, entry, slist_entry);
+        picotm_slist_enqueue_front(&self->allocated_entries,
+                                   &entry->slist_entry);
     }
 }
 
