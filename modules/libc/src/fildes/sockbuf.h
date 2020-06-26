86fda01eababddef6237333614e33b8bf1dca476
Thomas Zimmermann <contact@tzimmermann.org>
2020-06-26 19:56:57 +0200

    libc/fildes: Fix documentation of file buffers
    Signed-off-by: Thomas Zimmermann <contact@tzimmermann.org>

--- modules/libc/src/fildes/sockbuf.h
+++ modules/libc/src/fildes/sockbuf.h
@@ -1,6 +1,6 @@
 /*
  * picotm - A system-level transaction manager
- * Copyright (c) 2019   Thomas Zimmermann <contact@tzimmermann.org>
+ * Copyright (c) 2019-2020  Thomas Zimmermann <contact@tzimmermann.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -95,8 +95,8 @@ sockbuf_ref_or_set_up(struct sockbuf* self, int fildes,
  * \param       fildes      The sockbuf's sockbuf descriptor.
  * \param       id          The id to compare to.
  * \param[out]  error       Returns an error ot the caller.
- * \returns A value less than, equal to, or greater than if the ofd's id is
- *          less than, equal to, or greater than the given id.
+ * \returns A value less than, equal to, or greater than if the sockbuf's
+ *          id is less than, equal to, or greater than the given id.
  */
 int
 sockbuf_ref_or_set_up_if_id(struct sockbuf* self, int fildes,
